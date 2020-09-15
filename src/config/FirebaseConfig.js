2faf7a519ec9df9e2b232df5027bb38e4b35357e
Amol Nigdikar <35647064+Amolnigdikar@users.noreply.github.com>
2020-09-15 15:46:41 +0530

    update firebase config

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDv7jlOfxnYE9ztKDFasPpJXI0TGGx_irQ",
-  authDomain: "react-firebase-app-9fa5d.firebaseapp.com",
-  databaseURL: "https://react-firebase-app-9fa5d.firebaseio.com",
-  projectId: "react-firebase-app-9fa5d",
-  storageBucket: "react-firebase-app-9fa5d.appspot.com",
-  messagingSenderId: "1026704226746",
-  appId: "1:1026704226746:web:15b9f9377b82b46e56c583",
-  measurementId: "G-PYEDGNDX9H",
+  //paste your firebase config here
 });
 
 export const db = firebase.firestore();
